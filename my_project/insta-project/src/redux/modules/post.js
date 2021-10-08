2b396a5645d422faaa6029ed11cc0ee534293121
erionRK621 <jhkim62134@naver.com>
2021-10-08 21:51:06 +0900

    댓글 FB연결까지

--- my_project/insta-project/src/redux/modules/post.js
+++ my_project/insta-project/src/redux/modules/post.js
@@ -9,16 +9,20 @@ import { actionCreators as imageActions } from "./image";
 const SET_POST = "SET_POST";
 const ADD_POST = "ADD_POST";
 const EDIT_POST = "EDIT_POST";
+const LOADING = "LOADING";
 
-const setPost = createAction(SET_POST, (post_list) => ({ post_list }));
+const setPost = createAction(SET_POST, (post_list, paging) => ({ post_list, paging }));
 const addPost = createAction(ADD_POST, (post) => ({ post }));
 const editPost = createAction(EDIT_POST, (post_id, post) => ({
   post_id,
   post,
 }));
+const loading = createAction(LOADING, (is_loading) => ({ is_loading }));
 
 const initialState = {
   list: [],
+  paging: { start: null, next: null, size: 3 },
+  is_loading: false,
 };
 
 const initialPost = {
@@ -27,8 +31,7 @@ const initialPost = {
   //   user_name: "mean0",
   //   user_profile: "https://mean0images.s3.ap-northeast-2.amazonaws.com/4.jpeg",
   // },
-  image_url:
-    "https://s3.ap-northeast-2.amazonaws.com/erionrookie.shop/rookie.png",
+  image_url: "https://mean0images.s3.ap-northeast-2.amazonaws.com/4.jpeg",
   contents: "",
   comment_cnt: 0,
   insert_dt: moment().format("YYYY-MM-DD hh:mm:ss"),
@@ -150,16 +153,77 @@ const addPostFB = (contents = "") => {
   };
 };
 
-const getPostFB = () => {
+const getPostFB = (start = null, size = 3) => {
   return function (dispatch, getState, { history }) {
+
+    let _paging = getState().post.paging;
+
+    if(_paging.start && !_paging.next){
+      return;
+    }
+
+    dispatch(loading(true));
     const postDB = firestore.collection("post");
 
-    postDB.get().then((docs) => {
-      let post_list = [];
-      docs.forEach((doc) => {
-        let _post = doc.data();
+    let query = postDB.orderBy("insert_dt", "desc");
 
-        // ['commenct_cnt', 'contents', ..]
+    if(start){
+      query = query.startAt(start);
+    }
+
+
+    query
+      .limit(size + 1)
+      .get()
+      .then((docs) => {
+        let post_list = [];
+
+        let paging = {
+          start: docs.docs[0],
+          next: docs.docs.length === size+1? docs.docs[docs.docs.length -1] : null,
+          size: size,
+        }
+
+        docs.forEach((doc) => {
+          let _post = doc.data();
+
+          // ['commenct_cnt', 'contents', ..]
+          let post = Object.keys(_post).reduce(
+            (acc, cur) => {
+              if (cur.indexOf("user_") !== -1) {
+                return {
+                  ...acc,
+                  user_info: { ...acc.user_info, [cur]: _post[cur] },
+                };
+              }
+              return { ...acc, [cur]: _post[cur] };
+            },
+            { id: doc.id, user_info: {} }
+          );
+
+          post_list.push(post);
+        });
+
+        post_list.pop();
+
+        console.log(post_list);
+
+        dispatch(setPost(post_list, paging));
+      });
+  };
+};
+
+const getOnePostFB = (id) => {
+  return function(dispatch, getState, {history}){
+    const postDB = firestore.collection("post");
+    postDB
+      .doc(id)
+      .get()
+      .then((doc) => {
+        console.log(doc);
+        console.log(doc.data());
+
+        let _post = doc.data();
         let post = Object.keys(_post).reduce(
           (acc, cur) => {
             if (cur.indexOf("user_") !== -1) {
@@ -173,21 +237,32 @@ const getPostFB = () => {
           { id: doc.id, user_info: {} }
         );
 
-        post_list.push(post);
+        dispatch(setPost([post]));
       });
-
-      console.log(post_list);
-
-      dispatch(setPost(post_list));
-    });
-  };
-};
+  }
+}
 
 export default handleActions(
   {
     [SET_POST]: (state, action) =>
       produce(state, (draft) => {
-        draft.list = action.payload.post_list;
+        draft.list.push(...action.payload.post_list);
+
+        draft.list = draft.list.reduce((acc, cur) => {
+          if(acc.findIndex(a => a.id === cur.id) === -1){
+            return [...acc, cur];
+          }else{
+            acc[acc.findIndex((a) => a.id === cur.id)] = cur;
+            return acc;
+          }
+        }, []);
+
+
+        if(action.payload.paging){
+          draft.paging = action.payload.paging;
+        }
+        
+        draft.is_loading = false;
       }),
 
     [ADD_POST]: (state, action) =>
@@ -200,6 +275,9 @@ export default handleActions(
 
         draft.list[idx] = { ...draft.list[idx], ...action.payload.post };
       }),
+      [LOADING]: (state, action) => produce(state, (draft) => {
+        draft.is_loading = action.payload.is_loading;
+      })
   },
   initialState
 );
@@ -211,6 +289,7 @@ const actionCreators = {
   getPostFB,
   addPostFB,
   editPostFB,
+  getOnePostFB,
 };
 
 export { actionCreators };
--- my_project/insta-project/src/shared/firebase.js
+++ my_project/insta-project/src/shared/firebase.js
@@ -2,6 +2,7 @@ import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
+import "firebase/database";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCLgW5L9V4BFeO-__duiCWq8LcVDGV11zk",
@@ -19,5 +20,5 @@ const apiKey = firebaseConfig.apiKey;
 const auth = firebase.auth();
 const firestore = firebase.firestore();
 const storage = firebase.storage();
-
-export { auth, apiKey, firestore, storage };
+const realtime = firebase.database();
+export { auth, apiKey, firestore, storage, realtime };
